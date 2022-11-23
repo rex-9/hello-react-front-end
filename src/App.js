c4d677212bef9a2e2a30ece365d27d1cb695f8c1
Htet Naing <htetnaing0814@gmail.com>
2022-11-23 11:57:42 +0630

    fixed first review

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from './components/Greeting';
 
-const App = () => (
-  <>
-    <Greeting />
-  </>
-);
-
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+}
 export default App;
