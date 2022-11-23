6876ccd7139114d58e6af19dddd63d587295c3fa
Htet Naing <htetnaing0814@gmail.com>
2022-11-23 12:09:20 +0630

    removed comments

--- src/redux/greeting/greetingXer.js
+++ src/redux/greeting/greetingXer.js
@@ -16,19 +16,6 @@ export default (state = [], action) => {
 };
 
 export const fetchGreeting = createAsyncThunk(FETCH, async () => {
-  // fetch(`${baseURL}${API_URL}`, {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // })
-  // .then((response) => response.json().message)
-  // .then(data => {
-  //   console.log('Success:', data);
-  // })
-  // .catch((error) => {
-  //   console.error('Error:', error);
-  // });
-
   const response = await fetch(`${baseURL}${API_URL}`);
   const data = await response.json();
   return data.message;
